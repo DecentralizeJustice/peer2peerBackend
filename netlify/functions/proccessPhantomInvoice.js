a91b2b48f6b71161455c8563e2d91fbabaec8321
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-23 15:58:03 -0700

    add link to message

--- netlify/functions/proccessPhantomInvoice.js
+++ netlify/functions/proccessPhantomInvoice.js
@@ -9,10 +9,10 @@ const crypto = require('crypto')
 // const hri = require('human-readable-ids').hri
 const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
 const storeAddress = 'https://btcpay.anonshop.app/api/v1/stores/' + BTCpayStore + '/invoices/'
-/* const fs = require('fs')
+const fs = require('fs')
 const path = require("path")
-const pathWordlist = path.resolve(__dirname + "/bip39Wordlist.txt") */
-// const words = fs.readFileSync(pathWordlist, 'utf8').toString().split("\n")
+const pathWordlist = path.resolve(__dirname + "/bip39Wordlist.txt")
+const words = fs.readFileSync(pathWordlist, 'utf8').toString().split("\n")
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 const collection = client.db("real").collection("orders")
 const allPhoneInfo = client.db("real").collection("phonesInfo")
@@ -47,7 +47,6 @@ exports.handler = async (event) => {
     ) 
     const paymentInfo = paymentRequest.data
     delete orderInfo.storeId
-    console.log(orderInfo.metadata.purchase.serviceType)
 
     if (orderInfo.metadata.purchase.serviceType === '1service') {
       await process1Service(orderInfo, paymentInfo)
@@ -87,7 +86,8 @@ async function process1month(orderInfo, paymentInfo) {
   const firstMessage = {
     sender: 'dgoon',
     timestamp: Date.now(),
-    message: `Hi Friend. I have to configure your monthly rental. This configuration can take up to 24 hours. Message me here if you have any questions!`
+    message: `Hi Friend. I have to configure your monthly rental. This configuration can take up to 24 hours. 
+    Message me here if you have any questions! You can also check on your order here: `+ getCheckOrderLink(orderInfo.metadata.numberArray)
   }
   const docInfo = {
     passphrase: orderInfo.metadata.numberArray,
@@ -133,7 +133,7 @@ async function process1Service(orderInfo, paymentInfo) {
     sender: 'dgoon',
     timestamp: Date.now(),
     message: `Hi Friend. You should see the number for your service rental to the right. If you have any questions or need anything, shoot 
-    me a message here.`
+    me a message here. You can also check on your order here: ` + getCheckOrderLink(orderInfo.metadata.numberArray)
   }
   const docInfo = {
     passphrase: orderInfo.metadata.numberArray,
@@ -148,4 +148,19 @@ async function process1Service(orderInfo, paymentInfo) {
   const doc = docInfo
   await collection.insertOne(doc)
   return true
+}
+function getCheckOrderLink(numberArray){
+  const wordListFinal = numberArrayToWordArray(numberArray)
+  const link = 'https://phantomphone.app/login#' + wordListFinal.join(',')
+  return link
+}
+function numberArrayToWordArray (numberArray) {
+  const wordArray = []
+  const splitNumberArray = numberArray.split(",")
+  const length = splitNumberArray.length
+  for (var i=0;i<length; i++) {
+    const wordToAdd = words[splitNumberArray[i]]
+    wordArray.push(wordToAdd.replace(/(\r\n|\n|\r)/gm, ""))
+  }
+  return wordArray
 }
