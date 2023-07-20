6de81ead70f2470ea5b062f4e869d84f1041d9e3
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-07-20 14:04:01 -0400

    more logs

--- netlify/functions/proccessLockerOrder.js
+++ netlify/functions/proccessLockerOrder.js
@@ -29,7 +29,7 @@ exports.handler = async (event) => {
         }
       ) 
     const orderInfo = infoRequest.data
-
+    console.log(Date.now() - Number(orderInfo.metadata.timestamp))
     // less than 24 hours old
     if ((Date.now() - Number(orderInfo.metadata.timestamp)) > 86400000 || orderInfo.status !== 'Settled') {
       return {
